29ca8b6a65bfa0efb8168402e42eea0b400ab9f2
BhavaniPartha <bhavanivelan875@gmail.com>
2025-08-28 17:31:09 +0530

    Manage User php

--- manageUser.php
+++ manageUser.php
@@ -63,10 +63,12 @@ if (!empty($_SESSION['uname'])) {
   
   <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
-
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+
+  
   <style>
     .header {
       color: grey;
